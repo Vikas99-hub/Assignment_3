926e265402357c45d1fc2b578067aa351c18080b
Vikas H V <hvvikas89@gmail.com>
2026-01-21 20:11:06 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,10 @@
+import math
+
+number = int(input("Enter a number: "))
+square_root = math.sqrt(number)
+logarithm = math.log(number)
+sine = math.sin(number)
+
+print(f"The square root of a {number} is {square_root}")
+print(f"The logarithm of {number} is {logarithm}")
+print(f"The sine value of {number} is {sine}")
